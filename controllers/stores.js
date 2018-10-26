46fee1c99f3686fba878b98259b2a49df5e82f76
Tyana <tyana.stone@gmail.com>
2018-10-26 16:50:55 -0400

    added store controller to routes page

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,9 +1,9 @@
-const Store = require('../models/Stores')
+const Stores = require('../models/Stores')
 const User = require('../models/User')
 
 const storesController = {
     index: (req, res) => {
-        Store.find({}).populate('products')
+        Stores.find({}).populate('products')
             .then((stores) => {
                 // res.render sends to handlebars
                 res.render('stores/index', {
@@ -15,11 +15,11 @@ const storesController = {
         res.send("Hey this works")
     },
     show: (req, res) => {
-        const storeId = req.params.storesId
-        Store.findById(storeId).populate('products')
+        const storesId = req.params.storesId
+        Stores.findById(storesId).populate('products')
             .then((store) => {
                 // const products = store.products
-            res.render('stores/show', {store: store})
+            res.render('stores/show', {stores: stores})
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const applicationController = require('../controllers/application')
 const userController = require('../controllers/user')
+const storesController = require('../controllers/stores')
 
 router.get('/', applicationController.index)
 
 router.get('/', userController.index)
 
 
-
+router.get('/', storesController.index)
 
 module.exports = router
